448e94bbc50b0ef9222bc04befa00aa7a61c3f3b
Piyush Bhavsar <114468009+piyushbhavsarr@users.noreply.github.com>
2022-12-28 08:23:35 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,86 +1,80 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-void Bubble_sort(int arr[30],int n)
-{
-	int i,j,temp;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n-1;j++)
-		{
-			if(arr[j]>arr[j+1])
-			{
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-		}
-	}
-}
-void insertion_sort(int arr[30],int n)
-{
-	 int i ,j ,temp;
-	for(i=0;i<n;i++)
-	{
-		temp=arr[i];
-		j=i-1;
-		while(j>=0 && temp<=arr[j])
-		{
-			arr[j+1]=arr[j];
-			j=j-1;
-		}
-		arr[j+1]=temp;
-		
-	}
-}
-void fork1()
-{
-	int arr[25],arr1[25],n,i,status;
-	printf("Enter the number of values in array :");
-	scanf("%d",&n);
-	printf("Enter the array element :");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-	int pid=fork();
-	if(pid==0)
-	{
-		sleep(10);
-		printf("Child process \n");
-		printf("Child process id = %d\n",getpid());
-		insertion_sort(arr,n);
-      		printf("Element sorted using insertion sort :");
-		printf("\n");
-		for(i=0;i<n;i++)
-		{
-			printf("%d\t",arr[i]);
-		}
-		printf("\b");
-		printf("\n Parent Process id = %d\n",getppid());
-		system("ps -x");
-		printf("\n\n\n");
-	}
-	else
-	{
-		printf("Parent process \n");
-		printf("parent process id = %d \n",getppid());
-		Bubble_sort(arr,n);
-		printf("Element sorted using bubble sort:\n");
-		printf("\n");
-		for(i=0;i<n;i++)
-		{
-			printf("%d\t",arr[i]);
-			
-		}
-		printf("\n\n\n");
-	}
-}
-int main()
 
-{
-	fork1();
-	return 0;
+void bubble_sort(int *arr, int n);
+void insertion_sort(int *arr, int n);
+
+int main(void) {
+  int n;
+
+  
+  printf("Enter the number of integers to sort: ");
+  scanf("%d", &n);
+
+  
+  int arr[n];
+
+  
+  printf("Enter the integers: ");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  
+  pid_t child_pid = fork();
+
+  
+  if (child_pid == 0) {
+    
+    insertion_sort(arr, n);
+
+    printf("Sorted array (child process): ");
+    for (int i = 0; i < n; i++) {
+      printf("%d ", arr[i]);
+    }
+    printf("\n");
+  } else {
+    
+    bubble_sort(arr, n);
+
+    
+    wait(NULL);
+
+    printf("Sorted array (parent process): ");
+    for (int i = 0; i < n; i++) {
+      printf("%d ", arr[i]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
+
+
+void bubble_sort(int *arr, int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+
+void insertion_sort(int *arr, int n) {
+  for (int i = 1; i < n; i++) {
+    int key = arr[i];
+    int j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
 }
